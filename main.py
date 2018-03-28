e023086f40224169b58da3f79ddfa0d908cc1ccf
NSPro4w <nspro1@ocdsb.ca>
2018-03-28 11:26:34 -0400

    final variation

--- main.py
+++ main.py
@@ -3,26 +3,26 @@
 # <Nicholas Sprott>
 # March 28, 2018
 
-#1
-def CtoF (C):
+#1 conversion of celsius to fahrenheit
+def CtoF (C):#C for celsius
     """the purpose of this function is to convert the temperature from
        degrees celsius to degree fahrenheit
     """
 
     C = (1.8*C)+32
 
-    if C <= -459:
-        return 'it impossible to calculate'
+    if C <= -459: #this will determine if the temperature is below or above absolute zer0
+        return 'it impossible to calculate' 
     else:
         return round(C)
 
 
-#2
-def FtoC (F):
+#2 coversion of fahrenheit to celsius
+def FtoC (F):#F for fahrenheit
     """the pupose of this function is to convert F degreees to c degrees
     """
 
-    if F <= -459.4:
+    if F <= -459.4: #this will determine if the temperature is below or above absolute zer0
         return 'it is impossible to calculate'
     else:
         F = (0.55556)*(F-32)
@@ -33,30 +33,31 @@ def FtoC (F):
 CorF = int(input('input 1 for celsius to fahrenheit, or 2 for  fahrenheit  to celsius: '))
 
 #this is the code that will print the answer depending on the selected value of CorF
-
-if CorF == 1:
-    temperatureC = -273
-    while temperatureC < -272:
-      temperature = int(input('Enter your temperature in Celsius: '))
-      print(str(CtoF(temperature)) + ' degrees fahrenheit')
-      temperatureC = temperature
+while CorF != 0:
+  if CorF == 1:
+      temperatureC = -273#this variable is here so the while loop is able to begin
+      while temperatureC < -272:#while the temperature doesn't prduce a real answer
+        temperature = int(input('Enter your temperature in Celsius: '))#enter the temperature that will be converted
+        print(str(CtoF(temperature)) + ' degrees fahrenheit')#and print it if its input is above asolute 0
+        temperatureC = temperature
+    
 
 
       
-elif CorF == 2:
-    temperatureF = -459
-    while temperatureF < -458:
-        temperature = int(input('Enter your temperature in Fahrenheit: '))
-        print(str(FtoC(temperature)) + ' degrees celsius')
-        temperatureF = temperature
-
+  elif CorF == 2:
+      temperatureF = -459#this variable is here so the while loop is able to begin
+      while temperatureF < -458:#while the temperature doesn't prduce a real answer
+          temperature = int(input('Enter your temperature in Fahrenheit: '))#enter the temperature that will be converted
+          print(str(FtoC(temperature)) + ' degrees celsius')#and print it if its input is above asolute 0
+          temperatureF = temperature
     
-elif CorF != 1 and CorF != 2:
-    print('you stupid or something? i said 1 or 2, not whatever you think a 1 or 2 is. jk you smrt i only kid')
+        
+  elif CorF != 1 and CorF != 2:
+      print("sorry, that's an improper input.")
 
 
-
-
+  CorF = int(input("wanna try again, same inputs from before. or type 0 to stop the code: "))#this bit of code checks to if the inputer wants to run everything again
+                    
 
 
 
